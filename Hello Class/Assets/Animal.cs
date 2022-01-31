1d0ba736d337204cc54edc305af079e5e37f3046
iamksy98 <sy980407@gmail.com>
2022-01-31 19:32:58 +0900

    Initial commit

---
+++ Hello Class/Assets/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal
+{
+    public string name;
+    public string sound;
+
+    public void PlaySound()
+    {
+        Debug.Log(name + " : " + sound);
+    }
+
+}
+
+    
+
---
+++ Hello Coding/Assets/HelloCode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloCode : MonoBehaviour
+{
+    void Start(){
+        float distance = GetDistance(2, 2, 5, 6);
+        Debug.Log("(2.2)에서 (5,6)까지의 거리: " + distance);
+    }
+    float GetDistance(float x1, float y1, float x2, float y2)
+    {
+        float a = x2 - x1;
+        float b = y2 - y1;
+        float distance = Mathf.Sqrt(a * a + b * b);
+        return distance;
+    }
+    
+}
